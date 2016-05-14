f654993ffe905de25d98ccf2ca15dc3706732c0b
impomales <impomales@gmail.com>
2016-05-14 17:14:22 -0400

    add vector object position

--- FirstApp/Source.cpp
+++ FirstApp/Source.cpp
@@ -3,6 +3,10 @@
 using namespace std;	// allows to write cout
 						// instead of std::cout
 
+struct Vector {
+	float x, y, z;
+};
+
 struct Player
 {
 	int hp;
@@ -10,6 +14,7 @@ struct Player
 	char initialFirst;
 	char initialLast;
 	string name;
+	Vector position;
 };
 
 int main(void) {
@@ -18,6 +23,9 @@ int main(void) {
 	isaias.initialFirst = 'I';
 	isaias.initialLast = 'P';
 	isaias.name = "Isaias";
+	isaias.position.x = 1;
+	isaias.position.y = 2.25;
+	isaias.position.z = 3.75;
 	cout << "Hello, world" << endl;
 	cout << "I am programming in C++." << endl;
 	cout << "Status: " << endl;
@@ -26,5 +34,8 @@ int main(void) {
 	cout << "\tinitials: " << isaias.initialFirst;
 	cout << isaias.initialLast << endl;
 	cout << "\tname: " << isaias.name << endl;
+	cout << "\tposition: " << isaias.position.x;
+	cout << ", " << isaias.position.y << ", ";
+	cout << isaias.position.z << endl;
 	return 0;
 }
