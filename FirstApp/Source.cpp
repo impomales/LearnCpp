eab122a0de28935d417888fcaf9eca13a9844b3c
impomales <impomales@gmail.com>
2016-05-14 16:33:32 -0400

    hello world program

---
+++ FirstApp/Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>		// import io lib
+using namespace std;	// allows to write cout
+						// instead of std::cout
+
+int main(void) {
+	cout << "Hello, world" << endl;
+	cout << "I am programming in C++." << endl;
+	return 0;
+}
