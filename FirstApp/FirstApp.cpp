eb2aaea57c60e0e82aa529626a3ed62a6b035746
impomales <impomales@gmail.com>
2016-05-17 13:52:23 -0400

    add comments on debugger

--- FirstApp/FirstApp.cpp
+++ FirstApp/FirstApp.cpp
@@ -2,7 +2,12 @@
 #include <string>
 #include "math.h"
 
-
+// step into: executes next line of code.
+// if function call, steps into the function.
+// step over: executes all code in function.
+// step out: executes remaining code in current function.
+// run to cursor: executes up until cursor.
+// watch window: see values of variables changes as you step.
 
 using namespace std;	// allows to write cout
 						// instead of std::cout
@@ -28,6 +33,11 @@ int main(void) {
 
 	printNameAge(name, age);
 	cout << add(age, doubleNumber(age)) << endl;
+
+	age++;
+	age += 2;
+	age += 4;
+	age = doubleNumber(age);
 
 #ifdef PI
 	std::cout << PI << endl;
