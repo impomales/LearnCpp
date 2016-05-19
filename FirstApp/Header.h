632c409a91d9d2e0cf96c9b17dabb0d98574c42a
impomales <impomales@gmail.com>
2016-05-19 18:16:34 -0400

    external global constants

--- FirstApp/Header.h
+++ FirstApp/Header.h
@@ -3,9 +3,9 @@
 
 namespace constants
 {
-	const double pi(3.14159);
-	const double avogadro(6.0221413e23);
-	const double gravity(9.8);
+	extern const double pi;
+	extern const double avogadro;
+	extern const double gravity;
 }
 
 #endif
